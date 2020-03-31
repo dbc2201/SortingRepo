fb7615b54f464c6fd74d0519686af7472d18bc3a
dbc2201 <divyanshb22@gmail.com>
2020-03-31 10:53:32 +0530

    create an array

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,5 +15,6 @@ public class Main {
         final Scanner scanner = new Scanner(System.in);
         int numberOfElements = scanner.nextInt();
         scanner.close();
+        int[] array = new int[numberOfElements];
     }
 }
