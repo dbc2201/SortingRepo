1abd337d235e40be6c85f83a2546b7742924fd00
dbc2201 <divyanshb22@gmail.com>
2020-03-31 10:52:54 +0530

    prompt the user for number of elements in the array

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,8 +7,13 @@
 
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("How many elements would you like in the array?: ");
+        final Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        scanner.close();
     }
 }
