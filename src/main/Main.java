9c6131289802a3aed20e7e67996f7a8305031821
dbc2201 <divyanshb22@gmail.com>
2020-04-09 04:20:02 +0530

    create class QuickSort

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,7 +21,7 @@ public class Main {
         int numberOfElements = scanner.nextInt();
         int[] array = new int[numberOfElements];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100);
+            array[i] = (int) (Math.random() * 50);
         }
         System.out.println("UNSORTED ARRAY");
         System.out.println(Arrays.toString(array));
---
+++ src/quicksort/QuickSort.java
@@ -0,0 +1,11 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: dbc2201
+ *   Date: 09/04/20
+ *   Time: 4:19 AM
+ */
+
+package quicksort;
+
+public class QuickSort {
+}
