d83b3e907f65a440daa7c8ae38b319cb7b019f75
dbc2201 <divyanshb22@gmail.com>
2020-04-08 14:01:27 +0530

    code for InsertionSort

--- src/insertionsort/InsertionSort.java
+++ src/insertionsort/InsertionSort.java
@@ -8,4 +8,18 @@
 package insertionsort;
 
 public class InsertionSort {
+    public static void sort(int[] array) {
+        for (int nextIndex = 1; nextIndex < array.length; nextIndex++) {
+            insert(array, nextIndex);
+        }
+    }
+
+    private static void insert(int[] array, int nextIndex) {
+        int nextValue = array[nextIndex];
+        while (nextIndex > 0 && nextValue < array[nextIndex - 1]) {
+            array[nextIndex] = array[nextIndex - 1];
+            nextIndex--;
+        }
+        array[nextIndex] = nextValue;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@
 package main;
 
 import bubblesort.BubbleSort;
+import insertionsort.InsertionSort;
 import selectionsort.SelectionSort;
 
 import java.util.Arrays;
@@ -26,6 +27,7 @@ public class Main {
         System.out.println(Arrays.toString(array));
         System.out.println("Press 1 to sort this array using Bubble Sort");
         System.out.println("Press 2 to sort this array using Selection Sort");
+        System.out.println("Press 3 to sort this array using Insertion Sort");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1: /*BUBBLE SORT*/
@@ -40,6 +42,10 @@ public class Main {
                 System.out.println("ARRAY SORTED BY SELECTION SORT");
                 System.out.println(Arrays.toString(array));
                 break;
+            case 3: /*INSERTION SORT*/
+                InsertionSort.sort(array);
+                System.out.println("ARRAY SORTED BY INSERTION SORT");
+                System.out.println(Arrays.toString(array));
             default:
                 break;
         }
