76d53338195be27fe6e318cb03eda521db529de9
dbc2201 <divyanshb22@gmail.com>
2020-04-16 05:10:26 +0530

    add JavaDoc comment for the sort method

--- src/heapsort/HeapSort.java
+++ src/heapsort/HeapSort.java
@@ -9,6 +9,14 @@ package heapsort;
 
 public class HeapSort {
 
+    /**
+     * This method will sort an integer array using the Heap Sort Algorithm.
+     * NOTE: The Heap Sort Algorithm has a worst-case time complexity of O(n logn).
+     * pre-condition: The integer array named `array` must contain elements.
+     * post-condition: The integer array will be sorted.
+     *
+     * @param array The integer array to be sorted.
+     */
     public static void sort(int[] array) {
 
     }
